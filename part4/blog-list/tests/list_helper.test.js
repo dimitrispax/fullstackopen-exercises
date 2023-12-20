b6dec775efdca0beb8de01fd99e4570a860b9c75
dimitrispax <dimpax04@gmail.com>
2023-12-20 16:44:07 +0200

    finished exercise 4.6

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -1,13 +1,5 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
-
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
-})
-
-describe('total likes', () => {
     const emptyList = []
     const listWithOneBlog = [
       {
@@ -45,6 +37,14 @@ describe('total likes', () => {
           __v: 0
         },
         {
+          _id: "5a422b3a1b54a676234d17f3",
+          title: "Shortest path algorithms",
+          author: "Edsger W. Dijkstra",
+          url: "http://www.cs.amsterdam.edu/~EWD/shortest-path-algos.html",
+          likes: 12,
+          __v: 0
+        },
+        {
           _id: "5a422b891b54a676234d17fa",
           title: "First class tests",
           author: "Robert C. Martin",
@@ -69,17 +69,50 @@ describe('total likes', () => {
           __v: 0
         }  
     ]
-    test('of empty list is zero', () => {
-        const result = listHelper.totalLikes(emptyList)
-        expect(result).toBe(0)
+
+// test('dummy returns one', () => {
+//   const result = listHelper.dummy(blogs)
+//   expect(result).toBe(1)
+// })
+
+// describe('total likes', () => {
+
+//     test('of empty list is zero', () => {
+//         const result = listHelper.totalLikes(emptyList)
+//         expect(result).toBe(0)
+//       })
+//     test('when list has only one blog, equals the likes of that', () => {
+//       const result = listHelper.totalLikes(listWithOneBlog)
+//       expect(result).toBe(5)
+//     })
+//     test('of a bigger list is calculated right', () => {
+//         const result = listHelper.totalLikes(blogs)
+//         expect(result).toBe(36)
+//       })
+
+// })
+
+describe('favorite blog', () => {
+
+    test('of empty list is null', () => {
+        const result = listHelper.favouriteBlog(emptyList)
+        expect(result).toBe(null)
       })
-    test('when list has only one blog, equals the likes of that', () => {
-      const result = listHelper.totalLikes(listWithOneBlog)
-      expect(result).toBe(5)
+    test('when list has only one blog, this is the favourite', () => {
+      const result = listHelper.favouriteBlog(listWithOneBlog)
+      expect(result).toEqual({
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        likes: 5
+      })
     })
-    test('of a bigger list is calculated right', () => {
-        const result = listHelper.totalLikes(blogs)
-        expect(result).toBe(36)
+    test('of a bigger list, with equal likes, is the last one of the equals', () => {
+        const result = listHelper.favouriteBlog(blogs)
+        expect(result).toEqual({
+          title: "Shortest path algorithms",
+          author: "Edsger W. Dijkstra",
+          likes: 12
+        })
       })
-    
-  })
+      
+})
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -7,8 +7,26 @@ const dummy = (blogs) => {
     return blogs.reduce((totalLikes, blog) => totalLikes += blog.likes, 0)
   }
 
+  const favouriteBlog = (blogs) => {
+    if (blogs.length > 0) {
+        const maxObject = blogs.reduce((max, blog) => {
+            return max.likes > blog.likes ? max : blog  
+        })
+        return {
+            title : maxObject.title, 
+            author: maxObject.author, 
+            likes: maxObject.likes
+        }
+    }
+    else {
+        return null;
+    }
+    
+  }
+
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
   }
