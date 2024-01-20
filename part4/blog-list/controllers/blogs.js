9b7136f8138ece3a9fdbcaf0328498e1842361d4
dimitrispax <dimpax04@gmail.com>
2024-01-20 17:47:19 +0200

    finished exercise 4.14

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -2,11 +2,16 @@ const blogsRouter = require('express').Router()
 const { default: mongoose } = require('mongoose')
 const Blog = require('../models/blog')
 
+//////////////////////
+////////READ/////////
+////////////////////
 blogsRouter.get('/', async (request, response, next) => {
   const blogs = await Blog.find({})
   response.json(blogs)
 })
-
+//////////////////////
+///////CREATE////////
+////////////////////
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
@@ -41,6 +46,32 @@ blogsRouter.delete('/:id', async (request, response, next) => {
     response.status(404).end()
   }
 })
+//////////////////////
+///////UPDATE////////
+////////////////////
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
 
+  const newBlog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes ? body.likes : 0
+  }
+
+  /* Checking if id is valid */
+  if (mongoose.Types.ObjectId.isValid(request.params.id)) {
+    const res = await Blog.findByIdAndUpdate(request.params.id, newBlog, { new: true })
+    if (res !== null) // Checking if the id exists in DB.
+      response.status(200).json(newBlog)
+    else
+      response.status(404).end()
+  } else {  // if it isn't valid, throw 404.
+    response.status(404).end()
+  }
+
+
+
+})
 
 module.exports = blogsRouter
--- part4/blog-list/tests/bloglist_api.test.js
+++ part4/blog-list/tests/bloglist_api.test.js
@@ -81,31 +81,76 @@ beforeEach(async () => {
 //     .expect(400)
 // })
 
-test('data deletion is successful', async () => {
+// test('data deletion is successful', async () => {
+//   await api
+//     .delete('/api/blogs/65abd6aab205440f800f81bb')
+//     .expect(204)
+
+//   const res = await api.get('/api/blogs')
+//   expect(res.body).toHaveLength(helper.initialBlogs.length - 1)
+// })
+
+// test('data deletion is not successful because blog not found', async () => {
+//   await api
+//     .delete('/api/blogs/65abd6aab205440f800f81bc')
+//     .expect(404)
+
+//   const res = await api.get('/api/blogs')
+//   expect(res.body).toHaveLength(helper.initialBlogs.length)
+// })
+
+// test('data deletion is not successful because id is invalid', async () => {
+//   await api
+//     .delete('/api/blogs/325235235')
+//     .expect(404)
+
+//   const res = await api.get('/api/blogs')
+//   expect(res.body).toHaveLength(helper.initialBlogs.length)
+// })
+
+test('data update is successful', async () => {
+
+  const newBlog = {
+    "title": "TestBlog",
+    "author": "Vlakas Vlakeiou",
+    "url": "https://testblog.gr",
+    "likes": 3
+  }
   await api
-    .delete('/api/blogs/65abd6aab205440f800f81bb')
-    .expect(204)
+    .put('/api/blogs/65abd6aab205440f800f81bb')
+    .send(newBlog)
+    .expect(200)
 
   const res = await api.get('/api/blogs')
-  expect(res.body).toHaveLength(helper.initialBlogs.length - 1)
+  const updatedBlog = res.body.find(updatedBlog => updatedBlog.title === "TestBlog")
+  expect(updatedBlog.likes).toBe(3)
 })
 
-test('data deletion is not successful because blog not found', async () => {
-  await api
-    .delete('/api/blogs/65abd6aab205440f800f81bc')
-    .expect(404)
+test('data update is not successful because blog not found', async () => {
 
-  const res = await api.get('/api/blogs')
-  expect(res.body).toHaveLength(helper.initialBlogs.length)
+  const newBlog = {
+    "title": "TestBlog",
+    "author": "Vlakas Vlakeiou",
+    "url": "https://testblog.gr",
+    "likes": 3
+  }
+  await api
+    .put('/api/blogs/65abd6aab205440f800f81b2')
+    .send(newBlog)
+    .expect(404)
 })
 
-test('data deletion is not successful because id is invalid', async () => {
+test('data update is not successful because id is invalid', async () => {
+  const newBlog = {
+    "title": "TestBlog",
+    "author": "Vlakas Vlakeiou",
+    "url": "https://testblog.gr",
+    "likes": 3
+  }
   await api
-    .delete('/api/blogs/325235235')
+    .put('/api/blogs/2432423523')
+    .send(newBlog)
     .expect(404)
-
-  const res = await api.get('/api/blogs')
-  expect(res.body).toHaveLength(helper.initialBlogs.length)
 })
 
 afterAll(async () => {
